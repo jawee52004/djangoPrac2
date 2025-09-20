225408a43e20dbda17ddab55db5aad0b23694506
Elijah <jawee52004@gmail.com>
2025-09-20 15:07:51 +0800

    feature(myronchan): setup templates

--- djangoPrac2/myronchan/views.py
+++ djangoPrac2/myronchan/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def encouragement(request):
+    phrase = "Keep going, you’re doing great! 💪"
+    show_image = request.GET.get("show_image", False)
+    return render(request, "encouragement.html", {"phrase": phrase, "show_image": show_image})
